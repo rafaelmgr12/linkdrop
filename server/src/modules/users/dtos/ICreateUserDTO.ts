cd7986c72fe53a42eba65ddf91b5aa8440e9c8c9
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-10-17 19:10:02 -0300

    feat: add dto for user

---
+++ server/src/modules/users/dtos/ICreateUserDTO.ts
@@ -0,0 +1,7 @@
+interface ICreateUserDTO {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    id?: string;
+}
---
+++ server/src/modules/users/dtos/IUserResponseDTO.ts
@@ -0,0 +1,8 @@
+interface IUserResponseDTO {
+    email: string;
+    name: string;
+    username: string;
+    id: string;
+  }
+  
+  export { IUserResponseDTO };

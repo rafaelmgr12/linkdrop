65c91da302aabac8bcbd8df5a24e64bd972a2e89
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-10-19 15:46:10 -0300

    featÇ add delete by id feature

--- server/src/modules/users/provider/prisma/repository/UsersRepository.ts
+++ server/src/modules/users/provider/prisma/repository/UsersRepository.ts
@@ -1,25 +1,45 @@
-import{ prisma } from "config/prismaClient"
+import { prisma } from "config/prismaClient";
 import { ICreateUserDTO } from "modules/users/dtos/ICreateUserDTO";
 import { IUpdateUserDTO } from "modules/users/dtos/IUpdateUserDTO";
 import { IUsersRepository } from "modules/users/repositories/IUsersRepository";
 
-
 export class UsersRepository implements IUsersRepository {
-    create(data: ICreateUserDTO): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    update(data: IUpdateUserDTO): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    findByEmail(email: string): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    findByUsername(username: string): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
+  async create(data: ICreateUserDTO): Promise<void> {
+    await prisma.user.create({
+      data: {
 
+        name: data.name,
+        username: data.username,
+        email: data.email,
+        password: data.password,
 
+      },
+    });
+  }
+  async update(data: IUpdateUserDTO): Promise<void> {
+    await prisma.user.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        name: data.name,
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      },
+    });
+  }
+  async delete(id: string): Promise<void> {
+    await prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+  findByEmail(email: string): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+  findByUsername(username: string): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
 }

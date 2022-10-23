6eea1afcbbe1cd63e02fb10c535f11da7b93a796
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-22 23:43:51 -0300

    fix: change constructor

--- server/src/modules/users/useCases/signUp/SignUpController.ts
+++ server/src/modules/users/useCases/signUp/SignUpController.ts
@@ -1,18 +1,14 @@
 import { Request, Response } from "express";
+import { UsersRepository } from "modules/users/provider/prisma/repository/UsersRepository";
 import { IUsersRepository } from "modules/users/repositories/IUsersRepository";
 import { SignUpUseCase } from "./SignUpUseCase";
 
 export class SignUpController {
-    private usersRepository: IUsersRepository;
-    
-    constructor(usersRepository: IUsersRepository) {
-        this.usersRepository = usersRepository;
-    }
     
     async handle(request: Request, response: Response): Promise<Response> {
         const { name, username, email, password } = request.body;
     
-        const signUpUseCase = new SignUpUseCase(this.usersRepository);
+        const signUpUseCase = new SignUpUseCase(new UsersRepository);
 
         const user = await signUpUseCase.execute({
             name,

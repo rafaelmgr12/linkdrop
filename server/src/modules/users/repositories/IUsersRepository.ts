8776884e8dc3510160f4db86a5c61ba3a9cd3b2b
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-22 22:57:12 -0300

    fix: type return

--- server/src/modules/users/repositories/IUsersRepository.ts
+++ server/src/modules/users/repositories/IUsersRepository.ts
@@ -1,9 +1,10 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 import { IUpdateUserDTO } from "../dtos/IUpdateUserDTO";
+import { User } from "../entities/User";
 
 export interface IUsersRepository {
-  create(data: ICreateUserDTO): Promise<void>;
-  update(data: IUpdateUserDTO): Promise<void>;
-  findByEmail(email: string): Promise<any>;
-  findByUsername(username: string): Promise<any>;
+  create(data: ICreateUserDTO): Promise<User>;
+  update(data: IUpdateUserDTO): Promise<User | null>;
+  findByEmail(email: string): Promise<User>;
+  findByUsername(username: string): Promise<User>;
 }

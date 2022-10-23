b77900e8c384d9707da29e57f391a7bb5aad4353
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-22 23:25:04 -0300

    feat: add hash passwords

--- server/src/modules/users/useCases/signUp/SignUpUseCase.ts
+++ server/src/modules/users/useCases/signUp/SignUpUseCase.ts
@@ -2,27 +2,30 @@ import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "modules/users/dtos/ICreateUserDTO";
 import { User } from "modules/users/entities/User";
 import { IUsersRepository } from "modules/users/repositories/IUsersRepository";
+import { hash } from "bcrypt";
+export class SignUpUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
 
-export class SignUpUseCase{
-
-    constructor (private usersRepository: IUsersRepository){}
-
-
-    async execute(data: ICreateUserDTO): Promise<User> {
-
-        const emailAlreadyExists = await this.usersRepository.findByEmail(data.email);
-        if (emailAlreadyExists) {
-            throw new AppError("Email already registered");
-        }
-
-        const usernameAlreadyExists = await this.usersRepository.findByUsername(data.username);
-        if (usernameAlreadyExists) {
-            throw new AppError("Username already registered");
-        }
-        
-       const user = await this.usersRepository.create(data);
-
-       return user;
-       
+  async execute(data: ICreateUserDTO): Promise<User> {
+    const emailAlreadyExists = await this.usersRepository.findByEmail(
+      data.email
+    );
+    if (emailAlreadyExists) {
+      throw new AppError("Email already registered");
     }
+
+    const usernameAlreadyExists = await this.usersRepository.findByUsername(
+      data.username
+    );
+    if (usernameAlreadyExists) {
+      throw new AppError("Username already registered");
+    }
+
+    const hashPassword = await hash(data.password, 10);
+    data.password = hashPassword;
+
+    const user = await this.usersRepository.create(data);
+
+    return user;
+  }
 }

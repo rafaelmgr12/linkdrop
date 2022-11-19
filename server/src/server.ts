907acdbd4a441e0cd449c6eed8b2d47b9d774871
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-11-19 19:25:07 -0300

    feat: add error handler for promise

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,7 @@
-import express from "express";
+import { AppError } from "errors/AppError";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
 import { router } from "routes";
 
 const app = express();
@@ -6,5 +9,20 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+          message: err.message,
+        });
+      }
+      return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`,
+      });
+    }
+  );
+
 
 app.listen(3333, () => console.log("Server started on port 3333!"));
+

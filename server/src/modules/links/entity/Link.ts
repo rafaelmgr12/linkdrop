19214d076ade820e1574a82be9164600f1302642
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-11-21 21:11:45 -0300

    feat: add links models

--- server/src/modules/links/entity/Link.ts
+++ server/src/modules/links/entity/Link.ts
@@ -1,1 +1,28 @@
-class Link{}
+import crypto from "crypto";
+export class Link {
+  _id: string;
+  _title: string;
+  _url: string;
+  _description: string;
+  _created_at: Date;
+  _updated_at: Date;
+  _user_id: string;
+
+  constructor(
+    title: string,
+    url: string,
+    description: string,
+    user_id: string,
+    created_at?: Date,
+    updated_at?: Date,
+    id?: string
+  ) {
+    this._id = id ?? crypto.randomUUID().toString();
+    this._title = title;
+    this._url = url;
+    this._description = description;
+    this._user_id = user_id;
+    this._created_at = created_at ?? new Date();
+    this._updated_at = updated_at ?? new Date();
+  }
+}
